41d5bc764ad2cc6ac1309efd11e45ed771d170c7
VovaRuzhevich <vova.ruzhevich@gmail.com>
2021-07-24 22:01:23 +0300

    Adding contact_item for ContactsFragment

--- app/src/main/java/com/example/a2_contacts/MainActivity.kt
+++ app/src/main/java/com/example/a2_contacts/MainActivity.kt
@@ -1,11 +1,24 @@
 package com.example.a2_contacts
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        /*
+        ContentResolver resolver = getContentResolver();
+        mData = resolver.query(TVContract.Actors.CONTENT_URI, null, null, null, null);
+
+        int wordCol = cursor.getColumnIndex(DroidTermsExampleContract.COLUMN_WORD);
+        int definitionCol = cursor.getColumnIndex(DroidTermsExampleContract.COLUMN_DEFINITION);
+        while (cursor.moveToNext()) {
+        String word = cursor.getString(wordCol);
+        String definition = cursor.getString(definitionCol);
+        Log.v(“Cursor Example”, word + “ - “ + definition);
+        */
+
     }
 }
