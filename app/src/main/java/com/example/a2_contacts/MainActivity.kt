ba71aef33cd3b3738ee71b78c9cc3524f920011f
VovaRuzhevich <vova.ruzhevich@gmail.com>
2021-07-25 21:38:41 +0300

    Adding contacts fragment

--- app/src/main/java/com/example/a2_contacts/MainActivity.kt
+++ app/src/main/java/com/example/a2_contacts/MainActivity.kt
@@ -2,16 +2,31 @@ package com.example.a2_contacts
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val navHostFragment = supportFragmentManager
+            .findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        navController = navHostFragment.navController
+
+        //setupActionBarWithNavController(navController)
+        /*
+        override fun onSupportNavigateUp(): Boolean {
+            return navController.navigateUp() || super.onSupportNavigateUp()
+        }
+        */
         /*
         ContentResolver resolver = getContentResolver();
         mData = resolver.query(TVContract.Actors.CONTENT_URI, null, null, null, null);
-
+        //
         int wordCol = cursor.getColumnIndex(DroidTermsExampleContract.COLUMN_WORD);
         int definitionCol = cursor.getColumnIndex(DroidTermsExampleContract.COLUMN_DEFINITION);
         while (cursor.moveToNext()) {
@@ -19,6 +34,5 @@ class MainActivity : AppCompatActivity() {
         String definition = cursor.getString(definitionCol);
         Log.v(“Cursor Example”, word + “ - “ + definition);
         */
-
     }
 }
